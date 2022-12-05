dcfc260c805530604a8268662c8008bdd36a39a4
Aprialdilubis <aprialdilubis999@gmail.com>
2022-12-05 14:30:57 +0700

    menambahkan dropdown user

--- resources/views/layout/components/navbar.blade.php
+++ resources/views/layout/components/navbar.blade.php
@@ -26,24 +26,36 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-10 mb-lg-0">
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!"><i  class="bi bi-search"style="font-size:24px"></i></a></li>
-                <li class="nav-item"><a class="nav-link active" href="#"><i class="bi bi-plus-circle"style="font-size:24px"></i></a></li>
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!"><i class="bi bi-search"
+                            style="font-size:24px"></i></a></li>
+                <li class="nav-item"><a class="nav-link active" href="#"><i class="bi bi-plus-circle"
+                            style="font-size:24px"></i></a></li>
                 <li class="nav-item">
                     <a class="nav-link active" href="#">
                         <i class="bi bi-bookmark" style="font-size:24px"></i>
                     </a>
                 </li>
-                <li class="nav-item"><a class="nav-link active" href="/notification"><i class="bi bi-bell"style="font-size:24px"></i></i></a></li>
-                <li class="nav-item"><a class="nav-link active" href="/conversation"><i class="bi bi-chat-left-text"style="font-size:24px"></i></i></a></li>
-                <li class="nav-item"><a class="nav-link active" href="/user_profile"><i class="bi bi-person" style="font-size:24px"></i></a></li>
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" style="font-size:20px">Agus</a>
+                <li class="nav-item"><a class="nav-link active" href="/notification"><i class="bi bi-bell"
+                            style="font-size:24px"></i></i></a></li>
+                <li class="nav-item"><a class="nav-link active" href="/conversation"><i class="bi bi-chat-left-text"
+                            style="font-size:24px"></i></i></a></li>
+                <li class="nav-item"><a class="nav-link"></a></li>
+                    <ul class="navbar-nav" style="margin-top:1.5%">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button"data-bs-toggle="dropdown" aria-expanded="false" style="margin-left:10%">yourname&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                            <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarDarkDropdownMenuLink"
+                                style="width:1%">
+                                <li><a class="dropdown-item" href="/user_profile"><i class="bi bi-person-square"></i> My Account</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="bi bi-gear-fill"></i> Manage System</a></li>
+                                <div class="dropdown-divider"></div>
+                                <li><a class="dropdown-item" href="/home"><i class="bi bi-box-arrow-left"></i> Log Out</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
 
-                    <div class="dropdown-menu rounded-0 m-0">
-                        <a href="blog.html" class="dropdown-item">Blog Grid</a>
-                        <a href="single.html" class="dropdown-item">Blog Detail</a>
-                    </div>
-                </div>
+
             </ul>
         </div>
     </div>
