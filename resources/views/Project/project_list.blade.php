3cc4d3c5586172b20d60c078da5394e8f981d67b
Niken Novianti <niken.novianti@undiksha.ac.id>
2022-12-21 11:12:38 +0800

    list task

--- resources/views/Project/project_list.blade.php
+++ resources/views/Project/project_list.blade.php
@@ -1,5 +1,7 @@
 @extends('layout.master') @section('content')
 
+
+
 <!-- Page Content-->
 <section class="pt-4">
     <div class="container ">
@@ -15,9 +17,9 @@
               <a class="btn btn-secondary-outline me-auto btn" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
                 <i class="bi bi-stack"  style="color: #57575A;"></i><strong style="color: #57575A;">Group By</strong>
               </a>
-              <button class="btn btn-success me-3 border-0" style="background-color:#20B15A" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-plus-circle"></i> Add List</button>
-              <button class="btn btn-success me-3 border-0" style="background-color:#20B15A" data-bs-toggle="modal" data-bs-target="#exampleModalAddtask"><i class="bi bi-plus-circle"></i> Add Task</button>
-              <button class="btn btn-secondary me-3 border-0" style="background-color:#ffff"><i class="bi bi-gear-fill" style="color: #838492;"></i></button>
+              <button class="btn btn-success me-3" style="background-color:#20B15A" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-plus-circle"></i> Add List</button>
+              <button class="btn btn-success me-3" style="background-color:#20B15A" data-bs-toggle="modal" data-bs-target="#exampleModalAddtask"><i class="bi bi-plus-circle"></i> Add Task</button>
+              <button class="btn btn-secondary me-3" style="background-color:#ffff"><i class="bi bi-gear-fill" style="color: #838492;"></i></button>
               <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
                 <div class="offcanvas-header">
                   <h5 class="offcanvas-title" id="offcanvasExampleLabel"> <i class="bi bi-funnel-fill"  style="color: #57575A;"></i>&nbsp Filters</h5>
@@ -41,16 +43,24 @@
               </div>
             </div>
             <!--card list & task-->
-            <div class="col-lg-12 mb-3">
+            <div class="col-lg-12 col-xxl-6 mb-3">
                 <div class="card bg-light border-0 h-100">
                     <div class="card-body fs-6 text-sm-start p-8 p-lg-2 pt-0 pt-lg-0">
-                        <div style="width:100%;" class="btn bg-primary bg-gradient rounded-3 mb-3 mt-n3 text-start"><a href=""><span class="text-white">Analyst </a></i></div>
+                        <div style="width:1100px;" class="btn bg-primary bg-gradient rounded-3 mb-3 mt-n3 text-start"><a href=""><span class="text-white">Analyst </a></i></div>
                           @foreach ($project_tasks as $project_tasks)
                           <ul>
-                          <p class="fs-9 fw-bold text-start "><p class="" style="text-align: justify; margin-left: 1%; margin-right: 1%"><i class="bi bi-three-dots-vertical" style="margin-right:-2%;"></i><i class="bi bi-three-dots-vertical"style="margin-right: 1%"></i>{{$project_tasks ->title_task }} <mark style="background-color: #D0D9EE; color: #838492;">{{$project_tasks->status}}</mark><button type="button" class="btn btn-warning btn-sm" style="margin-left: 1%; margin-top: 1; color: #fff; ">{{ $project_tasks->priority }}</button><mark style="background-color: #D0D9EE;margin-left: 1%; color: #838492;">due </mark><mark style="color: #FF3D00; background-color: #D0D9EE;">may 30</mark><i class="bi bi-flag-fill btn" style="color : #3766DF"></i><a class="bi bi-chat-left btn" href="discussopd.html"style="color: #A3A4A7; margin-left: -1%;"></a></p>
-                          </ul>
+                                <p class="fs-9 fw-bold text-start "><p class="" style="text-align: justify; margin-left: 1%; margin-right: 1%"><i class="bi bi-three-dots-vertical" style="margin-right:-2%;"></i><i class="bi bi-three-dots-vertical"style="margin-right: 1%"></i>{{$project_tasks ->title_task }} <mark style="background-color: #D0D9EE; color: #838492;">{{$project_tasks->status}}</mark><button type="button" class="btn btn-warning btn-sm" style="margin-left: 1%; margin-top: 1; color: #fff; ">{{ $project_tasks->priority }}</button><mark style="background-color: #D0D9EE;margin-left: 1%; color: #838492;">due </mark><mark style="color: #FF3D00; background-color: #D0D9EE;">may 30</mark><i class="bi bi-flag-fill btn" style="color : #3766DF"></i><a class="bi bi-chat-left btn" href="discussopd.html"style="color: #A3A4A7; margin-left: -1%;">
+                                <a href="" class="badge bg-danger" style="margin-right:1%;">Edit</a>
+                                <form action="/project_tasks/{{$project_tasks ->title_task }}" method="post" class="d-inline">
+                                @method('delete')
+                                @csrf
+                              <button class="badge bg-danger border-0" onclick="return confirm('Yakin hapus data')" style="margin-right:1%;">Hapus</button>
+                                </form>
+                              </a></p>
+                            </ul>
                             @endforeach
                         </div>
+                        
                     </div>
                 </div>
             <a class="h6 text-decoration-none text-success mb-5" href="" data-bs-toggle="modal" data-bs-target="#exampleModalAddtask"><i class="bi bi-plus-circle-fill"></i> Add Task</a>
@@ -69,9 +79,9 @@
                     @csrf
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Title </label>
-                        <input type="text" name="tile" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <input type="text" name="title" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                       </div>
-                    <div class="mb-3">
+                  <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Description</label>
                         <textarea type="text" name="list_description" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></textarea>
                       </div>
@@ -102,13 +112,13 @@
               <div class="row mb-3">
                 <label for="inputPassword3" class="col-sm-3 col-form-label">Task List</label>
               <div class="col-sm-9">
-                <input type="text" name="#" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="text" name="task_list" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
               </div>
               </div>
                 <div class="row mb-3">
                   <label for="inputPassword3" class="col-sm-3 col-form-label">Title</label>
                   <div class="col-sm-9">
-                    <input type="text" name="title_task" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <input type="text" name="title_task" value ="title" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                   </div>
                 </div>
                 <div class="mb-3">
@@ -119,34 +129,33 @@
                     <div class="col">
                       <label for="inputEmail4" class="form-label">Assigned To</label>
                       <select name="status" class="form-control">
-                        <option value="">User 1</option>
-                        <option value="">User 2</option>
-                        <option value="">User 3</option>
+                        <option value="User 1">User 1</option>
+                        <option value="User 2">User 2</option>
+                        <option value="User 3">User 3</option>
                       </select>
                     </div>
                     <div class="col">
                       <label for="inputEmail4" class="form-label">Start Date</label>
-                      <input type="date" class="form-control" placeholder="Start Date" aria-label="Start Date">
+                      <input type="date" class="form-control" name="start_date" placeholder="Start Date" aria-label="Start Date">
                     </div>
                     <div class="col">
                       <label for="inputEmail4" class="form-label">Due Date</label>
-                      <input type="date" class="form-control" placeholder="Due Date" aria-label="Due Date">
+                      <input type="date" class="form-control" name="due_date" placeholder="Due Date" aria-label="Due Date">
                     </div>
                     <div class="col">
                       <label for="inputEmail4" class="form-label">Status</label>
                       <select name="status" class="form-control">
-                        <option value="">No Progress</option>
-                        <option value="">In Progres</option>
-                        <option value="">Complate</option>
+                        <option value="No Progress">No Progress</option>
+                        <option value="In Progres">In Progres</option>
+                        <option value="Complate">Complate</option>
                       </select>
                     </div>
                     <div class="col">
                       <label for="inputEmail4" class="form-label">Priority</label>
                       <select name="priority" class="form-control">
-                        <option value="">none</option>
-                        <option value="">low</option>
-                        <option value="">medium</option>
-                        <option value="">high</option>
+                        <option value="low">low</option>
+                        <option value="medium">medium</option>
+                        <option value="high">high</option>
                       </select>
                     </div>
                   </div>
@@ -170,5 +179,6 @@
       </div>
     </div>
   </div>
+  
 </section>
 @endsection
